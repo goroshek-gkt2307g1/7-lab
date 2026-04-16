30d6c0d53a6df401647f787c4cbcfb8aa16ef23a
Goroshek <goroshek@example.com>
2026-04-17 00:08:18 +0700

    пример Стратегия

---
+++ IFormatStrategy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_lab
+{
+	public interface IFormatStrategy
+	{
+		string Format(string message, DateTime timestamp);
+	}
+}
---
+++ TextFormatStrategy.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_lab
+{
+	public class TextFormatStrategy : IFormatStrategy
+	{
+		public string Format(string message, DateTime timestamp) =>
+			$"[{timestamp:yyyy-MM-dd HH:mm:ss}] {message}";
+	}
+}
